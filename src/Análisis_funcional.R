c5df7ef2b81bca6d2fd9685adf9f841c6b581e22
Andttrea <andreavg@lcg.unam.mx>
2026-05-30 22:32:28 -0600

    Move DAVIDChartReport CSVs into results/anotacion/data and add Análisis_funcional.R

---
+++ src/Análisis_funcional.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(dplyr)
+library(stringr)
+library(forcats)
+
+up_go <- read.csv("/export/storage/users/andreavg/ProyectoFinal_transcriptomica/results/anotacion/data/DAVIDChartReport_upregulated_BP.csv", header = TRUE)
+down_go <- read.csv("/export/storage/users/andreavg/ProyectoFinal_transcriptomica/results/anotacion/data/DAVIDChartReport_downregulated_BP.csv", header = TRUE)
