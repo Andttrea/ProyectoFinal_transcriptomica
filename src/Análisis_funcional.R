2a35efb04c96196bbd604cf94d5db3796f0df31f
Andttrea <andreavg@lcg.unam.mx>
2026-05-30 22:32:48 -0600

    se añadio script

--- src/Análisis_funcional.R
+++ src/Análisis_funcional.R
@@ -5,3 +5,4 @@ library(forcats)
 
 up_go <- read.csv("/export/storage/users/andreavg/ProyectoFinal_transcriptomica/results/anotacion/data/DAVIDChartReport_upregulated_BP.csv", header = TRUE)
 down_go <- read.csv("/export/storage/users/andreavg/ProyectoFinal_transcriptomica/results/anotacion/data/DAVIDChartReport_downregulated_BP.csv", header = TRUE)
+
